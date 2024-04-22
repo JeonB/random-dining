ffffd28d9b4ad28fa68b4922e7cedc4fa436de7c
sojeon <sojeon@ifinfo.co.kr>
2024-04-22 13:08:08 +0900

    @env 설정 변경 - app.config.ts 에 환경변수 명세

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,15 @@
-import { KAKAO_RESTAPI_KEY } from '@env';
-import { Alert, Linking } from 'react-native';
-import * as Location from 'expo-location';
-import { QueryParamsType } from 'types/queryParams';
-
-const baseUrl = 'https://dapi.kakao.com/v2/local/search/keyword';
-const restAPIkey = KAKAO_RESTAPI_KEY;
+// import { KAKAO_RESTAPI_KEY } from '@env'
+import { Alert, Linking } from 'react-native'
+import * as Location from 'expo-location'
+import { QueryParamsType } from '@_types/queryParams'
+import Constants from 'expo-constants'
+import { AppConfig } from 'app.config'
+const { KAKAO_RESTAPI_KEY } = Constants.expoConfig?.extra as AppConfig
+const baseUrl = 'https://dapi.kakao.com/v2/local/search/keyword'
+const restAPIkey = KAKAO_RESTAPI_KEY
 
 async function getLocation() {
-  const { status } = await Location.requestForegroundPermissionsAsync();
+  const { status } = await Location.requestForegroundPermissionsAsync()
   if (status !== 'granted') {
     Alert.alert(
       '위치 권한이 필요합니다.',
@@ -20,17 +22,17 @@ async function getLocation() {
         { text: '예', onPress: () => Linking.openSettings() },
       ],
       { cancelable: false },
-    );
-    return { latitude: undefined, longitude: undefined };
+    )
+    return { latitude: undefined, longitude: undefined }
   }
   const location = await Location.getCurrentPositionAsync({
     accuracy: Location.Accuracy.Lowest,
-  });
+  })
 
   return {
     latitude: location.coords.latitude.toString(),
     longitude: location.coords.longitude.toString(),
-  };
+  }
 }
 
 export async function fetchData(
@@ -38,10 +40,10 @@ export async function fetchData(
   page: number,
   distanceRange: number,
 ) {
-  const { latitude, longitude } = await getLocation();
+  const { latitude, longitude } = await getLocation()
 
-  const defaultCategories = ['한식', '중식', '일식', '양식', '분식'];
-  let results = [];
+  const defaultCategories = ['한식', '중식', '일식', '양식', '분식']
+  let results = []
 
   if (!query && latitude && longitude) {
     for (let category of defaultCategories) {
@@ -51,8 +53,8 @@ export async function fetchData(
         latitude,
         longitude,
         distanceRange,
-      );
-      results.push(result);
+      )
+      results.push(result)
     }
   } else if (query && latitude && longitude) {
     results = await fetchSingleCategoryData(
@@ -61,10 +63,10 @@ export async function fetchData(
       latitude,
       longitude,
       distanceRange,
-    );
+    )
   }
 
-  return results;
+  return results
 }
 
 async function fetchSingleCategoryData(
@@ -82,25 +84,25 @@ async function fetchSingleCategoryData(
     radius: distanceRange,
     size: 15,
     page,
-  };
+  }
 
   const queryString = Object.keys(queryParams)
     .map(
       key =>
         `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`,
     )
-    .join('&');
-  const url = `${baseUrl}?${queryString}`;
+    .join('&')
+  const url = `${baseUrl}?${queryString}`
   const response = await fetch(url, {
     method: 'GET',
     headers: {
       Authorization: `KakaoAK ${restAPIkey}`,
     },
-  });
+  })
   if (!response.ok) {
-    throw new Error('Response 실패');
+    throw new Error('Response 실패')
   }
 
-  const data = await response.json();
-  return data;
+  const data = await response.json()
+  return data
 }
