3e083cf2dab573682b7ae0a67ca009c256991cdb
sojeon <sojeon@ifinfo.co.kr>
2024-05-17 10:46:13 +0900

    식당 관련 스택 스크린 추가

---
+++ src/components/restaurantStackScreen.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import FilterSetting from '@_3Rpages/FilterSettings/filterSetting'
+import DetailView from '@_3Rpages/RestaurantView/detailView'
+import SelectedRestaurantInfo from '@_3Rpages/RestaurantView/selectedRestaurantInfo'
+import PositionSelector from '@_3Rpages/positionSelector'
+import MapSearch from '@_3Rpages/RestaurantView/mapSearch'
+import { createStackNavigator } from '@react-navigation/stack'
+import { RestaurantParamList } from '@_types/restaurantParamList'
+import { Ionicons, AntDesign } from '@expo/vector-icons'
+
+const RestaurantStackScreen = () => {
+  const Stack = createStackNavigator<RestaurantParamList>()
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={PositionSelector}
+        options={{ title: '홈 화면', headerBackTitleVisible: false }}
+      />
+      <Stack.Screen
+        name="MapSearch"
+        component={MapSearch}
+        options={{
+          title: '지도에서 선택한 위치로 추천 받기',
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <AntDesign
+              name="back"
+              size={30}
+              color="midnightblue"
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="FilterSetting"
+        component={FilterSetting}
+        options={{
+          title: '필터 설정',
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <AntDesign
+              name="back"
+              size={30}
+              color="midnightblue"
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="SelectedRestaurantInfo"
+        component={SelectedRestaurantInfo}
+        options={{
+          title: '',
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <AntDesign
+              name="back"
+              size={30}
+              color="midnightblue"
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailView}
+        options={{
+          title: '상세 화면',
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <AntDesign
+              name="back"
+              size={30}
+              color="midnightblue"
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
+export default RestaurantStackScreen
