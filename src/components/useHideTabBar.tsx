ebb92f7a8740dbe1df3b26e35da61d4867ea90d3
MJ/jin20 <mjkim@ifinfo.co.kr>
2024-05-28 17:49:00 +0900

    useHideTabBar 훅 속도 문제 해결

--- src/components/useHideTabBar.tsx
+++ src/components/useHideTabBar.tsx
@@ -1,20 +1,22 @@
-import { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { useCallback } from 'react'
 
 export const useHideTabBar = () => {
   const navigation = useNavigation()
 
-  useLayoutEffect(() => {
-    // 부모 네비게이션의 옵션 설정
-    navigation.getParent()?.setOptions({
-      tabBarStyle: { display: 'none' },
-    })
-
-    // 컴포넌트가 언마운트 될 때 탭바를 다시 보이게 설정
-    return () => {
+  useFocusEffect(
+    useCallback(() => {
+      // 부모 네비게이션의 옵션 설정
       navigation.getParent()?.setOptions({
-        tabBarStyle: { display: 'flex' },
+        tabBarStyle: { display: 'none' },
       })
-    }
-  }, [navigation])
+
+      // 컴포넌트가 언포커스될 때 탭바를 다시 보이게 설정
+      return () => {
+        navigation.getParent()?.setOptions({
+          tabBarStyle: { display: 'flex' },
+        })
+      }
+    }, [navigation]),
+  )
 }
