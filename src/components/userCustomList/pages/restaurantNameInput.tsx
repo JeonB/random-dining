2de005d19be0c29b5df528c21d7d130364bde9e0
MJ/jin20 <mjkim@ifinfo.co.kr>
2024-05-30 17:50:39 +0900

    클릭시 패딩 보임, 기기마다 간격 다름 문제 수정

--- src/components/userCustomList/pages/restaurantNameInput.tsx
+++ src/components/userCustomList/pages/restaurantNameInput.tsx
@@ -1,9 +1,17 @@
 import React, { Dispatch, SetStateAction, useState } from 'react'
-import { Alert, Dimensions, StyleSheet, TextInput, View } from 'react-native'
+import {
+  Alert,
+  Dimensions,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import { Icon } from '@rneui/themed'
 import { LocationTypes } from '@_types/restaurant'
 import { MyTheme } from 'theme'
-
+import { Feather } from '@expo/vector-icons'
 interface Props {
   listItems: LocationTypes[]
   setListItems: Dispatch<SetStateAction<LocationTypes[]>>
@@ -21,7 +29,6 @@ export const RestaurantNameInput: React.FC<Props> = ({
       Alert.alert('이미 리스트에 있는 식당 이름입니다.')
       return
     }
-
     // 입력 필드에 입력된 값을 리스트에 추가
     setListItems(prevItems => [
       ...prevItems,
@@ -34,26 +41,43 @@ export const RestaurantNameInput: React.FC<Props> = ({
   const addIconColor = inputRestaurant ? MyTheme.colors.primary : 'gray'
   return (
     <View style={styles.restaurantInputContainer}>
-      <TextInput
-        style={styles.restaurantNameField}
-        placeholder="식당 또는 메뉴 이름을 입력하세요."
-        onChangeText={setInputRestaurant}
-        value={inputRestaurant}
-        testID="restaurantNameField"
-        onSubmitEditing={handlePressRestaurantAddButton}
-        returnKeyType="done"
-        clearButtonMode="always"
-      />
-      <Icon
-        name="add"
-        size={22}
-        color={addIconColor}
-        onPress={handlePressRestaurantAddButton}
-        testID="restaurantAddButton"
-        style={styles.addIcon}
-        disabled={inputRestaurant.trim() === ''}
-        disabledStyle={{ backgroundColor: 'white' }}
-      />
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.restaurantNameField}
+          placeholder="식당 또는 메뉴 이름을 입력하세요."
+          onChangeText={setInputRestaurant}
+          value={inputRestaurant}
+          testID="restaurantNameField"
+          returnKeyType="done"
+          clearButtonMode="always"
+          onSubmitEditing={() => {
+            if (inputRestaurant.trim() !== '') {
+              handlePressRestaurantAddButton()
+            }
+          }}
+        />
+        {Platform.OS === 'android' && inputRestaurant ? (
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 15,
+            }}
+            onPress={() => setInputRestaurant('')}>
+            <Feather name="x" size={15} color="black" />
+          </TouchableOpacity>
+        ) : null}
+      </View>
+      <View style={styles.iconContainer}>
+        <Icon
+          name="add"
+          size={22}
+          color={addIconColor}
+          onPress={handlePressRestaurantAddButton}
+          testID="restaurantAddButton"
+          disabled={inputRestaurant.trim() === ''}
+          disabledStyle={{ backgroundColor: 'white' }}
+        />
+      </View>
     </View>
   )
 }
@@ -62,21 +86,25 @@ const { width, height } = Dimensions.get('window')
 const styles = StyleSheet.create({
   restaurantInputContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
   },
   restaurantNameField: {
-    width: '85%',
+    flex: 1,
     borderColor: 'black',
     borderWidth: 1,
     borderRadius: 10,
     padding: 12,
-    fontSize: 17,
+    fontSize: width > 360 ? 17 : 15,
+    marginRight: 5,
   },
-  addIcon: {
-    flex: 1,
-    flexDirection: 'row',
+  iconContainer: {
+    justifyContent: 'center',
     borderWidth: 1,
     borderRadius: 10,
-    paddingHorizontal: 10,
+    padding: 10,
+  },
+  inputArea: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 })
