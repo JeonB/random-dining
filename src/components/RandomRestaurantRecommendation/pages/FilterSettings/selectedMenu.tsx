25c51d1e3662a1db5825770b910ede09db492a9e
sojeon <sojeon@ifinfo.co.kr>
2024-06-23 19:58:39 +0900

    이미지 컴포넌트 분리

--- src/components/RandomRestaurantRecommendation/pages/FilterSettings/selectedMenu.tsx
+++ src/components/RandomRestaurantRecommendation/pages/FilterSettings/selectedMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer, useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import {
   View,
   Image,
@@ -16,25 +16,7 @@ import { Button } from 'react-native-paper'
 import { useStore } from '@_common/utils/zustandStore'
 import { Text } from '@rneui/themed'
 import { fetchLocationData } from '@_services/api'
-
-interface DataType {
-  [key: string]: any
-}
-const images: DataType = {
-  스테이크:
-    'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  파스타:
-    'https://img.freepik.com/free-photo/spaghetti-meatballs_1339-3006.jpg?t=st=1718776607~exp=1718780207~hmac=e6e20b55b90e90832626e51a25fbfcdaa405b5975e0674b306a1e56cc5e7e210&w=996',
-  피자: 'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  햄버거:
-    'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  라자냐:
-    'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  샐러드:
-    'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  뇨키: 'https://img.freepik.com/free-photo/fried-steak-pieces-wooden-board-garlic_140725-4719.jpg?t=st=1718776404~exp=1718780004~hmac=be7c6b242989d8a511cf8f259d9a6795085baedab170180fe9693eeebc025565&w=826',
-  // ... 나머지 이미지들
-}
+import { images } from './images'
 
 export const SelectedMenu = ({
   route,
