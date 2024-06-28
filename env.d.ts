e732a38d412d23a172ecfd721547eb3faf387bab
sojeon <sojeon@ifinfo.co.kr>
2024-06-28 10:50:56 +0900

    ios , android 빌드 ignore 처리

--- env.d.ts
+++ env.d.ts
@@ -1,13 +1,12 @@
 // env.d.ts
 declare namespace NodeJS {
   interface ProcessEnv {
-    APP_ENV: 'development' | 'production' | 'preview'
-    STORYBOOK_ENABLED: 'true' | 'false'
+    APP_ENV: string
+    STORYBOOK_ENABLED: string
     KAKAO_RESTAPI_KEY: string
     KAKAO_JAVASCRIPT_KEY: string
     KAKAO_NATIVEAPP_KEY: string
     PROD_ANDROID_BANNER_ID: string
     PROD_IOS_BANNER_ID: string
-    // Add any other environment variables here
   }
 }
